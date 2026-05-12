e50645e19a7291337cc4faaaaaa5ed5c1128c053
Adrian Donato <adriandonato9@gmail.com>
2026-05-11 20:12:35 -0500

    Add --preload to gunicorn to avoid cold start delays

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 python manage.py migrate --noinput
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --preload
